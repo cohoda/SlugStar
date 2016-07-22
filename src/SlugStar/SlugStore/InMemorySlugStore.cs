02872c00c2a8c5f2ffbbab5f161412d6ff31e462
Alex Brown <Alex Brown>
2016-07-22 13:28:17 +0100

    created inMemorySlugStore

---
+++ src/SlugStar/SlugStore/InMemorySlugStore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+
+namespace SlugStar.SlugStore
+{
+    public class InMemorySlugStore : ISlugStore
+    {
+        internal static ConcurrentBag<Slug> Cache = new ConcurrentBag<Slug>();
+
+        public bool Exists(string slug)
+        {
+            return Cache.Any(x => x.Value == slug);
+        }
+
+        public void Store(Slug slug)
+        {
+            Cache.Add(slug);
+        }
+    }
+}
---
+++ tests/SlugStar.Tests/SlugStore/InMemorySlugStoreTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SlugStar.Tests.SlugStore
+{
+    [TestFixture]
+    public class InMemorySlugStoreTests
+    {
+        private InMemorySlugStore _inMemorySlugStore;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _inMemorySlugStore = new InMemorySlugStore();
+            InMemorySlugStore.Cache = new ConcurrentBag<Slug>();
+        }
+
+        [Test]
+        public void Store_Adds_slug_value_to_collection()
+        {
+            _inMemorySlugStore.Store(new Slug("Store_Adds_slug_value_to_collection"));
+
+            var result = InMemorySlugStore.Cache.Any(x => x.Value == "Store_Adds_slug_value_to_collection");
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Exists_Returns_true_if_slug_value_exists()
+        {
+            InMemorySlugStore.Cache.Add(new Slug("Exists_Returns_true_if_slug_value_exists"));
+
+            var result = _inMemorySlugStore.Exists("Exists_Returns_true_if_slug_value_exists");
+
+            Assert.That(result, Is.True);
+        }
+
+
+        [Test]
+        public void Exists_Returns_false_if_slug_value_does_not_exist()
+        {
+            var result = _inMemorySlugStore.Exists("Exists_Returns_false_if_slug_value_does_not_exist");
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
