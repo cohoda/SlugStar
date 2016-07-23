05cd60adbefc18c04f5f80421cd4aa71d503fe74
Alex Brown <Alex Brown>
2016-07-23 11:16:17 +0100

    added created dateTime

--- src/SlugStar/Slug.cs
+++ src/SlugStar/Slug.cs
@@ -1,12 +1,17 @@
-﻿namespace SlugStar
+﻿using System;
+
+namespace SlugStar
 {
     public class Slug
     {
         public Slug(string value)
         {
             Value = value;
+            Created = DateTime.Now;
         }
 
         public string Value { get; set; }
+
+        public DateTime Created { get; set; }
     }
 }
